b00815c49c3f4f0e68bbebc65dedcf146f3d9830
AlexRzh <a.rzhevtsev@netrika.ru>
2019-01-09 17:26:14 +0300

    Initial commit

---
+++ src/Colors.java
@@ -0,0 +1,12 @@
+public class Colors
+{
+    public static void main(String[] args) {
+
+        String colors[] = {"Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Violet"};
+
+        for(int i = (colors.length - 1); i >= 0; i--)
+        {
+            System.out.println(colors[i]);
+        }
+    }
+}
